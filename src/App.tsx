8f458686eaf5bc24bf58c353769a90c4d22e2097
Grin0502 <akarihiroshi@heyneo.so>
2025-08-13 20:53:40 +0900

    Frontend initialize

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+import ClientView from './view/client';
+
+function App() {
+  return (
+    <ClientView />
+  );
+}
+
+export default App;
---
+++ src/layout/client/header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div className="w-full h-[200px] bg-gray-500">
+            <div className="flex justify-center items-center color-gray text-[50px] max-w-[1200px] h-full m-auto">
+                Header
+            </div>
+        </div>
+    );
+};
+
+export default Header;
